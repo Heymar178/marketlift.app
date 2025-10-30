c5e16a9b723f4922055bedddc83e0573c1ba5fca
Heymar178 <hamzaworks178@gmail.com>
2025-10-30 17:26:07 -0500

    Simplify rendering by removing routing
    Removed routing components and simplified rendering.

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-import PrivacyPolicy from "./PrivacyPolicy";
-import TermsOfService from "./TermsOfService";
-import NotFound from "./NotFound";
-
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/terms-of-service" element={<TermsOfService />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
