a3c432898411b42794dd1e06227ca360247e4b42
Heymar178 <hamzaworks178@gmail.com>
2025-10-30 17:38:57 -0500

    Simplify main.tsx by removing routing
    Removed routing setup and unused imports.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,5 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// NOTE: these files must exist with exact casing:
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfService from "./pages/TermsOfService";
-
-// If you don't have this yet, create a simple one (see step 4)
-import NotFound from "./pages/NotFound";
-
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/terms-of-service" element={<TermsOfService />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
